4ec99c79a4ab198d09ebab7c32170fd7b7f541bc
jessp <jessp@10.120.23.154>
2018-09-26 10:53:18 -0400

    Working on test conditions

--- Stacks/src/ArrayPalindrome.java
+++ Stacks/src/ArrayPalindrome.java
@@ -1,4 +1,52 @@
+import java.util.Stack;
 
 public class ArrayPalindrome {
+
+	Stack<Character> one;
+	Stack<Character> two;
+	Stack<Character> three;
+
+	public ArrayPalindrome() {
+
+		one = new Stack<>();
+		two = new Stack<>();
+		three = new Stack<>();
+
+	}
+
+	public boolean isPalindrome(String match) {
+		
+		
+		for (int i = 0, j = match.length() ; i < j; i++) {
+			
+			
+		
+			one.push(match.charAt(i));
+			two.push(match.charAt(i));
+			
+			System.out.print(match.charAt(i));
+
+		}
+		for (int i = 0; i < match.length(); i++) {
+			three.push(two.pop());
+
+		}
+		for (int i = 0; i < match.length() ; i++) {
+			char look = one.peek();
+			char secondLook = three.peek();
+
+			if (look != secondLook) {
+
+				System.out.println(" This is not a palindrome ");
+				return false;
+
+			}
+			one.pop();
+			three.pop();
+
+		}
+		System.out.println(" This is a palindrome");
+		return true;
+	}
 
 }
---
+++ Stacks/src/PalindromeTest.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class PalindromeTest {
+
+	public static void main(String[] args) {
+		
+		ArrayPalindrome test = new ArrayPalindrome();
+		
+		test.isPalindrome("racecar");
+		test.isPalindrome("mom");
+		test.isPalindrome("bible");
+		test.isPalindrome("dad");
+		test.isPalindrome("Eva can I stab bats in a cave");
+		test.isPalindrome("atinmugforajarofgumnita");
+		test.isPalindrome("welcome");
+		test.isPalindrome("book");
+		
+		
+	}
+
+}
